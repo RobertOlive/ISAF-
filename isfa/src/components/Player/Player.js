b340a1bf3dc043e7d356cc09651d97a3a5c9554f
Robert Olive <robert.aaron.olive@gmail.com>
2019-09-05 23:15:35 -0500

    the audio player works! still needs functionality and is currently hard-wired to play the first episode only

--- isfa/src/components/Player/Player.js
+++ isfa/src/components/Player/Player.js
@@ -2,14 +2,28 @@ import React, {Component} from "react";
 import "./Player.css"
 
 class Player extends Component {
+
+    playClick () {
+        if (document.getElementsByClassName("pause").length===0) {
+            document.getElementById("triangle").classList.add("pause")
+            document.getElementById("triangle").classList.remove("triangle");
+            document.getElementById("audio").play();
+        } else if (document.getElementsByClassName("pause").length>0) {
+            document.getElementById("triangle").classList.remove("pause")
+            document.getElementById("triangle").classList.add("triangle")
+            document.getElementById("audio").pause();
+        }
+    }
+
     render() {
-        console.log(this.props);
+        console.log(this.props.episodes? this.props.episodes[0].enclosure.link:"hey");
         return(
             <div className="player row">
                 <div className="playBtn">
-                    <div className="triangle"></div>
+                    <div id="triangle" className="triangle" onClick={this.playClick}></div>
                 </div>
                 <div className="currentTitle col">{this.props.episodes? this.props.episodes[0].title: "loading episode..."}</div>
+                <audio id="audio" src={this.props.episodes? this.props.episodes[0].enclosure.link: "none"}/>
             </div>
         )
     }
