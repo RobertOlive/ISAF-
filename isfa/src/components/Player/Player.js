523f464815fc4e61a31221545f0b97559efa3728
Robert Olive <robert.aaron.olive@gmail.com>
2019-10-04 20:02:23 -0500

    adding tracklist

--- isfa/src/components/Player/Player.js
+++ isfa/src/components/Player/Player.js
@@ -32,23 +32,30 @@ class Player extends Component {
     render() {
         console.log(this.state.currentEp);
         return(
-            <div className="player row">
-                <div className="playBtn">
-                    <div id="triangle" className="triangle" onClick={this.playClick}></div>
-                </div>
-                <div className="currentTitle col">
-                    {this.state.currentEp? this.state.currentEp.title: "loading episode..."}
-                    <br/>
-                    <progress 
-                        id="progressBar" 
-                        value="0" max={this.state.currentEp? this.state.currentEp.enclosure.duration: "100"}
+            <div className="row">
+                <div className="col-12 player">
+                    <div className="playBtn">
+                        <div id="triangle" className="triangle" onClick={this.playClick}></div>
+                    </div>
+                    <div className="currentTitle col">
+                        {this.state.currentEp? this.state.currentEp.title: "loading episode..."}
+                        <br/>
+                        <progress 
+                            id="progressBar" 
+                            value="0" max={this.state.currentEp? this.state.currentEp.enclosure.duration: "100"}
+                        />
+                    </div>
+                    <audio 
+                        id="audio" src={this.state.currentEp? this.state.currentEp.enclosure.link:"none"}
+                        onTimeUpdate={this.initProgressBar}
+                        loop={false}
                     />
                 </div>
-                <audio 
-                    id="audio" src={this.state.currentEp? this.state.currentEp.enclosure.link:"none"}
-                    onTimeUpdate={this.initProgressBar}
-                    loop={false}
-                />
+                <div className="col-12 trackList">
+                    {this.props.episodes.map((episode)=> {
+                        return (<div>{episode.title}</div>)
+                    })}
+                </div>
             </div>
         )
     }
